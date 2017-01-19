2ab915599da51ce37faed57135915cb37043d02f
PatrickSunico <apatricksunico@gmail.com>
2017-01-19 15:15:01 +0800

    Update from different branch

--- public/includes/sections/showcase.php
+++ public/includes/sections/showcase.php
@@ -1,45 +1,76 @@
 <section class="section-spacing">
   <h2 id="services" class="h2 underlined__black">Showcase.</h2>
-    <div class="row row-spacing">
 
-      <!-- separated with columns -->
-      <div class="six columns column-spacing">
-        <div class="thumb-unit">
-          <img src="assets/Dining.jpg" alt="" class="img-portfolio">
-            <div class="thumb-overlay"></div>
+
+  <div class="row-spacing">
+    <div class="cards">
+      <!-- cards__item padding container for individual grid -->
+
+      <div class="cards__item">
+
+        <div class="card">
+          <!-- Card BG -->
+          <div class="card__image">
+            <img class="img-portfolio" src="assets/Couch-set.jpg" alt="">
+            <div class="tint__overlay"></div>
             <div class="text-overlay">
-              <h3 class="h3 underlined">Dining Set</h3>
-              <p>
-                Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit,
-              </p>
-              <p>
-                Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit,
-              </p>
-              <p><b>Height:</b> 45 inches </p>
-              <p><b>Width:</b> 120 inches </p>
-              <p><b>Weight:</b> 140 lb </p>
-              <div class="btn-position">
-                <a class="ghost-button" href="#"> View More </a>
-              </div>
+              <h3 class="h3"> Test 123</h3>
             </div>
+          </div>
+
+          <!-- Description -->
+          <div class="card__description">
+            <div class="card__title">Flex</div>
+            <p class="card__text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+          </div>
         </div>
+
       </div>
 
-      <div class="six columns column-spacing">
-        <div href="#" class="thumb-unit">
-          <img src="assets/Couch-set.jpg" alt="" class="img-portfolio">
-            <div class="thumb-overlay"></div>
+      <div class="cards__item">
+
+        <div class="card">
+          <!-- Card BG -->
+          <div class="card__image">
+            <img class="img-portfolio" src="assets/Couch-set.jpg" alt="">
+            <div class="tint__overlay"></div>
             <div class="text-overlay">
-              <h3 class="h3 underlined">Couch Set</h3>
-              <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Mauris blandit aliquet elit, </p>
-              <p><b>Height:</b> 45 inches </p>
-              <p><b>Width:</b> 120 inches </p>
-              <p><b>Weight:</b> 140 lb </p>
-              <div class="btn-position">
-                <a class="ghost-button" href="#"> View More </a>
-              </div>
+              <h3 class="h3"> Test 123</h3>
             </div>
+          </div>
+
+          <!-- Description -->
+          <div class="card__description">
+            <div class="card__title">Flex</div>
+            <p class="card__text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+          </div>
         </div>
+
       </div>
+
+      <div class="cards__item">
+
+        <div class="card">
+          <!-- Card BG -->
+          <div class="card__image">
+            <img class="img-portfolio" src="assets/Couch-set.jpg" alt="">
+            <div class="tint__overlay"></div>
+            <div class="text-overlay">
+              <h3 class="h3"> Test 123</h3>
+            </div>
+          </div>
+
+          <!-- Description -->
+          <div class="card__description">
+            <div class="card__title">Flex</div>
+            <p class="card__text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
+  </div>
+
+
 </section>
