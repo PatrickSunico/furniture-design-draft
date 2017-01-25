5ffa1de424c031787c0858d47fa117cb59a20b66
PatrickSunico <apatricksunico@gmail.com>
2017-01-25 16:54:44 +0800

    Update

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -5,6 +5,6 @@
       </div> <!-- /#wrapper -->
       <!-- jQuery -->
       <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-      <script src="/js/scripts.js" charset="utf-8"></script>
+      <script src="js/scripts.js" charset="utf-8"></script>
     </body>
   </html>
